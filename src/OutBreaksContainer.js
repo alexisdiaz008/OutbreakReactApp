e496836e9a4a9d81ac7642a1cdbecd3826a95278
alexisdiaz008 <lexmasta@gmail.com>
2020-02-14 20:50:08 -0500

    OutBreaksContainer component mockup

---
+++ src/OutBreaksContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class OutBreaksContainer extends Component {
+  render() {
+    return (
+      <div>
+        OutBreaks Container
+      </div>
+    )
+  }
+}
+
+export default OutBreaksContainer
