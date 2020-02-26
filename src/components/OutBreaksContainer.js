58b70775726cedda819b55b4567ad4bfd2589a67
alexisdiaz <lexmasta@gmail.com>
2020-02-26 00:01:00 -0500

    styled google map

--- src/components/OutBreaksContainer.js
+++ src/components/OutBreaksContainer.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import { Map, GoogleApiWrapper } from 'google-maps-react';
+import GoogleMapComponentWithMarker from './GoogleMapWithMarker'
+
+// Some default styles
+const styles = {
+  width: '100%',
+  height: '536px'
+}
 
 export class OutBreaksContainer extends Component {
   render() {
-  	return (
-  	  <Map
-  	    google={this.props.google}
-  	    zoom={14}
-  	    className="map-styles"
-  	    initialCenter={{
-  	     lat: -1.2884,
-  	     lng: 36.8233
-  	    }}
-  	  />
-  	);
+    return (
+      <div style={styles}>
+        <GoogleMapComponentWithMarker
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key="
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    )
   }
 }
 
